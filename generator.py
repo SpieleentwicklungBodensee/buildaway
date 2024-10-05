ac78577911dbf863cb73f816c0b9b069fd879aa0
kikomedia <45310068+kikomedia@users.noreply.github.com>
2024-10-05 13:51:53 +0200

    added generator logic

--- generator.py
+++ generator.py
@@ -1,11 +1,21 @@
-import perlin_noise
+from perlin_noise import PerlinNoise
+
+class GeneratorData(object):
+    def __init__(self, level, width, height):
+        self.level = level + 473284
+        self.width = width
+        self.height = height
+        self.grid = [[' ' for x in range(width)] for y in range(height)] 
 
 class Generator(object):
     def __init__(self):
+
+        self.noise = PerlinNoise(octaves=10, seed=1)
         pass
 
     def run(self, level, width, height):
-        return  ['          P                                                             ',
+        if level == -1:
+            return  ['          P                                                             ',
                  '                                                                        ',
                  '                                                          ##            ',
                  'GGGGGGGGGGGG                                                            ',
@@ -17,3 +27,92 @@ class Generator(object):
                  'FFFFF      GGGFFG        FFFFF                                      GGFF',
                  'FFFFF~~~~~~FFFFFF~~~~~~~~FFFFF~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FFFF',
                  ]
+
+        data = GeneratorData(level, width, height)
+
+        self.make_water(data)
+
+        self.make_floors(data)
+
+        self.make_it_green(data)
+
+        self.make_player(data)
+
+        # make each line as string
+        for y in range(data.height):
+            data.grid[y] = ''.join(data.grid[y])
+
+        return data.grid
+                   
+    def change_block(self, data, x, y, element):
+        try:
+            data.grid[y][x] = element
+        except IndexError:
+            pass
+
+    def make_floors(self, data):
+        ratio =   (data.width / data.height) 
+
+        # lower generation
+        threshold = 3
+        for y in range(data.height):
+            for x in range(data.width):
+                xdim = x / 80 
+                ydim = y 
+                cur = self.noise([xdim, ydim, data.level/10]) * 20
+                if cur > threshold: 
+                    #print (cur)
+                    #if data[y][x] != '~':
+                    self.change_block(data, x, y+5, 'F')
+
+        # fillup blocks from below
+        for x in range(data.width):
+            lowest = -1
+            for y in reversed(range(data.height)):
+                if data.grid[y][x] in ['F', 'G']:
+                    lowest = y
+                    break
+            
+            if lowest > -1:
+                for y in range(lowest, data.height):
+                    self.change_block(data, x, y, 'F')
+
+        # make more platforms above
+        threshold = 3
+        for y in range(data.height):
+            for x in range(data.width):
+                xdim = x / 80  + 20
+                ydim = y / 10
+                cur = self.noise([xdim, ydim, data.level/10+1234]) * 20
+                if cur > threshold and y < 7: 
+                    #print (cur)
+                    #if data[y][x] != '~':
+                    self.change_block(data, x, y, 'F')
+
+    def make_water(self, data):
+        for x in range(data.width):
+            data.grid[-1][x] = '~'
+
+    def make_it_green(self, data,):
+        for x in range(data.width):
+            last = ' '
+            for y in range(data.height):
+                cur = data.grid[y][x]
+
+                if cur == 'F' and last == ' ':
+                    self.change_block(data, x, y, 'G')
+                
+                last = cur
+
+    def make_player(self, data,):
+        for x in range(data.width):
+            if x > 5:
+                last = ''
+                for y in range(data.height):
+                    cur = data.grid[y][x]
+
+                    if cur == 'G' and last == ' ':
+                        self.change_block(data, x, y-1, 'P')
+                        return
+                    last = cur
+                   
